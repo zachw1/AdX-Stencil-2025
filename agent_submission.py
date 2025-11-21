404013ffdaa9bd4044c611a80f36946ac0da2bf1
zach <zachary_z_wang@brown.edu>
2025-11-20 21:05:59 -0500

    agent4

--- agent_submission.py
+++ agent_submission.py
@@ -3,6 +3,6 @@ from .agent2 import TrialNDaysNCampaignsAgent
 from .agent4 import MyNDaysNCampaignsAgent as Agent4
 
 ################### ACTUAL SUBMISSION #####################
-# agent_submission = Agent4(name="clash_royale")
-agent_submission = TrialNDaysNCampaignsAgent(name="brawl_stars", shade_param=0.4)
+agent_submission = Agent4(name="clash_royale")
+#agent_submission = TrialNDaysNCampaignsAgent(name="brawl_stars", shade_param=0.4)
 ###########################################################
